2365f2728c7c0977308b7fce7f6b0e473bc32c96
JD Skinner <yu.kagi@gmail.com>
2018-10-17 10:47:24 -0600

    End of Part 6: Function Clauses

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,9 +2,12 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
+
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     [method, path, _] =
@@ -17,7 +20,17 @@ defmodule Servy.Handler do
   end
 
   def route(conv) do
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
     %{ conv | resp_body: "Bears, Lions, Tigers" }
+  end
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+  end
+  def route(conv, "GET", "/bigfoot") do
+    %{ conv | resp_body: "Bigfoot" }
   end
 
   def format_response(conv) do
@@ -40,5 +53,31 @@ Accept: */*
 
 """
 
+request2 = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request3 = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
 response = Servy.Handler.handle(request)
+IO.puts response
+
+IO.puts("====================================")
+
+response = Servy.Handler.handle(request2)
+IO.puts response
+
+IO.puts("====================================")
+
+response = Servy.Handler.handle(request3)
 IO.puts response
