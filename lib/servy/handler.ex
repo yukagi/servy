4e10de0e3c8a87e10df2d62313a89ae32d88476d
JD Skinner <yu.kagi@gmail.com>
2018-10-18 09:42:17 -0600

    Wrapped up Part 9: Serve Static Files

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -42,14 +42,39 @@ defmodule Servy.Handler do
   def route(%{method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
+  def route(%{method: "GET", path: "/bears/new"} = conv) do
+    IO.puts "==============================="
+    IO.puts "inside the bears new route function"
+
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("form.html")
+    |> File.read
+    |> handle_file(conv)
+  end
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
   def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
     %{conv | status: 403, resp_body: "Cannot delete da bears"}
   end
+  def route(%{method: "GET", path: "/about"} = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
+  end
   def route(%{ path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+  def handle_file({:error, :enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found!"}
+  end
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File Error: #{reason}"}
   end
 
   def format_response(conv) do
@@ -131,6 +156,21 @@ Accept: */*
 
 """
 
+request8 = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+request9 = """
+GET /bears/new HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
 response = Servy.Handler.handle(request)
 IO.puts response
 
@@ -156,6 +196,14 @@ IO.puts response
 
 IO.puts("====================================")
 response = Servy.Handler.handle(request7)
+IO.puts response
+
+IO.puts("====================================")
+response = Servy.Handler.handle(request8)
+IO.puts response
+
+IO.puts("====================================")
+response = Servy.Handler.handle(request9)
 IO.puts response
 
 
