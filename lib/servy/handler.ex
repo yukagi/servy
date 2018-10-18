b76a660ba74c49e9a287f9179699406ca3aedf51
JD Skinner <yu.kagi@gmail.com>
2018-10-18 08:31:29 -0600

    Finished Part 8: Rewrite Paths and Track 404s

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,9 +1,12 @@
 defmodule Servy.Handler do
+
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
 
@@ -19,24 +22,34 @@ defmodule Servy.Handler do
     %{ method: method, path: path, resp_body: "", status: nil }
   end
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"} 
   end
+  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+    %{conv | path: "/bears/" <> id} 
+  end
+  def rewrite_path(conv), do: conv
 
-  def route(conv, "GET", "/wildthings") do
-    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "WARNING: #{path} is on the loose."
+    conv
   end
-  def route(conv, "GET", "/bears") do
-    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  def track(conv), do: conv
+
+  def route(%{method: "GET", path: "/wildthings"} = conv ) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
-  def route(conv, "GET", "/bears/" <> id) do
-    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
-  def route(conv, "DELETE", "/bears/" <> id) do
-    %{ conv | status: 200, resp_body: "Bear #{id} has been deleted" }
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
   end
-  def route(conv, _method, path) do
-    %{ conv | status: 404, resp_body: "No #{path} here!" }
+  def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{conv | status: 403, resp_body: "Cannot delete da bears"}
+  end
+  def route(%{ path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
@@ -102,25 +115,47 @@ Accept: */*
 
 """
 
+request6 = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request7 = """
+GET /bears?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
 response = Servy.Handler.handle(request)
 IO.puts response
 
 IO.puts("====================================")
-
 response = Servy.Handler.handle(request2)
 IO.puts response
 
 IO.puts("====================================")
-
 response = Servy.Handler.handle(request3)
 IO.puts response
 
 IO.puts("====================================")
-
 response = Servy.Handler.handle(request4)
 IO.puts response
 
 IO.puts("====================================")
-
 response = Servy.Handler.handle(request5)
 IO.puts response
+
+IO.puts("====================================")
+response = Servy.Handler.handle(request6)
+IO.puts response
+
+IO.puts("====================================")
+response = Servy.Handler.handle(request7)
+IO.puts response
+
+
